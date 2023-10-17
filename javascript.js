76d69da757ce16a9fe8e237c0e01c5f73a164bfd
hoomanhorsey <94696321+hoomanhorsey@users.noreply.github.com>
2023-10-17 11:44:59 +1100

    Second draft of rps UI, now refactored to separate functions and incl. restart btn

--- javascript.js
+++ javascript.js
@@ -2,141 +2,122 @@
 // Wait for page to load
 document.addEventListener("DOMContentLoaded", function() {
 
-let ties = 0;
-let humanWins = 0;
-let computerWins = 0;
+// Display opening scene/hide others
+document.querySelector('.selectionDiv').style.display = 'none';   
+document.querySelector('.resetDiv').style.display = 'none';
 
-// Selecting buttons and triggering game once button is clicked
-let btn = document.querySelectorAll('button');
+// Start game
+let startButton = document.querySelector('.startButton');
+startButton.addEventListener('click', () => {
 
-btn.forEach(function(e){
+// Switch to selection scene
+document.querySelector('.openingDiv').style.display = 'none';
+document.querySelector('.selectionDiv').style.display = 'block';
 
+let scoreboard = {tie: 0, humanWins: 0, computerWins: 0};
+
+let playerChoice = '';
+
+// Activate selection buttons
+let btn = document.querySelectorAll('.selectButton'); 
+
+    btn.forEach(function(e){
     e.addEventListener('click', () =>
         {
         // Extracting player choice from selected button
-        let playerChoice = e.innerHTML.toLowerCase();
-        
-        // Selecting result div           
-        resultsDiv = document.querySelector('.results');               
-        // Clear result div           
-        resultsDiv.innerHTML = "";
-        
-        result = document.createElement('div');
+        playerChoice = e.innerHTML.toLowerCase();
 
         let computerChoice = getComputerChoice();
 
         // Initiate round
         let gameResult = playRound(playerChoice, computerChoice);
 
-        // Calculate Tally
-                     
-    
+        scoreBoard = tallyScore(gameResult, scoreboard);
 
-        if (gameResult === "T")
-        {
-            console.log(gameResult);
+        let parsedResult = parseResultMessage(gameResult);
 
-            ties += 1;
-            console.log("ties", ties);
+        // Selecting result div           
+        resultsDiv = document.querySelector('.results');               
+        // Clear result div           
+        resultsDiv.innerHTML = "";
+        result = document.createElement('p');
 
-        }
-        else if (gameResult === "H")
-        {
-            console.log(gameResult);
-
-            humanWins += 1;
-            console.log("human", humanWins);
-        }
-        else 
-        {
-            console.log(gameResult);
-
-            computerWins += 1;
-            console.log("computer", computerWins);
-
-        }
-     
-        console.log("ties: " + ties + ", human " + humanWins + ", computer: " + computerWins)
-
+        resultsDiv.appendChild(
+            createResultMessage(playerChoice, 
+                                computerChoice, 
+                                scoreboard, 
+                                parsedResult));
         
-        if (gameResult === "T") 
-            {
-                gameResult = "Tie" 
-            } 
-            else if (gameResult === "H") 
-            {
-                gameResult = "Human Wins"
-            }
-            else {gameResult = "Computer Wins"}
+        if ((scoreboard['tie'] == 5) || (scoreboard['humanWins'] == 5) || (scoreboard['computerWins'] ==5)) {
+        overallWinner(scoreboard);
+        document.querySelector('.selectionDiv').style.display = 'none';
+        document.querySelector('.resetDiv').style.display = 'block';
 
-            console.log(gameResult);
-
-
-
-        // Insert Tallying Switch case
-
-        resultsMessage = `Human selected: ${playerChoice} <br>
-        Computer selected: ${computerChoice}<br><br> 
-        <strong>${gameResult}</strong><br><br>
-        Tally: Ties:  ${ties}, Human wins: ${humanWins} and Computer wins: ${computerWins}`
+        let resetBtn = document.querySelector('.resetBtn');
+        resetBtn.addEventListener('click', () => {
+            location.reload();
+        });
+        }   
+        })
+        })
+    })
+});
 
 
-        if ((ties != 5) && (humanWins != 5) && (computerWins !=5)) {
-            result.innerHTML =  resultsMessage;
-            resultsDiv.appendChild(result);
-
-        } else {
-            if (ties ===5) {
-            result.innerHTML = `${resultsMessage} <br><br> <strong>All tied!</strong>`;
-            resultsDiv.appendChild(result);
-
-            
- 
-            } else if (humanWins === 5) {
-                result.innerHTML = `${resultsMessage} <br><br> <strong>Human is the Overall Winner!</strong>`; 
-                resultsDiv.appendChild(result);
-
-
-            } else {
-                result.innerHTML = `${resultsMessage} <br><br> <strong>Computer is the Overall Winner!</strong>`;             }
-                resultsDiv.appendChild(result);
-
-
-        }
+function overallWinner(scoreboard){
+    if (scoreboard['tie'] === 5) {
        
-        
-                  })
-    });
+        result.innerHTML = `${resultsMessage} <br><br> <strong>All tied!</strong>`;
+        return result;      
 
-        
-// Create score tally
-
-// Get user selection
-// Generate computer selection
-// Simulate fight!
-
-// Add result to tally
-// Display result
-
-function getTally(gameResult)
-{
-    
-
-    if (gameResult == "It's a tie!")
-        {
-        ties += 1;
+        } else if (scoreboard['humanWins']  === 5) {
+            result.innerHTML = `${resultsMessage} <br><br> <strong>Human is the Overall Winner!</strong>`; 
+            return result;
+        } else {           
+            result.innerHTML = `${resultsMessage} <br><br> <strong>Computer is the Overall Winner!</strong>`;             }
+            return result;
         }
-    else if (gameResult == "Human is the Winner!")
-        {
-            humanWins += 1;
-        }
+ 
+function tallyScore(gameResult, scoreboard) {
+    if (gameResult === "T")
+    {
+        scoreboard['tie'] += 1;
+    }
+    else if (gameResult === "H")
+    {
+        scoreboard['humanWins'] += 1;
+    }
     else 
-        {
-            computerWins += 1;
-        }
-     
-}
+    {
+        scoreboard['computerWins'] += 1;
+    }
+    return scoreboard;
+    }
 
+function parseResultMessage(gameResult) {
+   
+    if (gameResult === "T") 
+        {
+            return "Tie" 
+        } 
+        else if (gameResult === "H") 
+        {
+            return "Human Wins"
+        }
+        else {
+            return "Computer Wins"
+            }
+    }
+
+function createResultMessage(playerChoice, computerChoice, scoreboard, parsedResult) {
+    resultsMessage = `Human selected: ${playerChoice} <br>
+    Computer selected: ${computerChoice}<br><br> 
+    <strong>${parsedResult}</strong><br><br>
+    Tally: Ties:  ${scoreboard['tie']}, Human wins: ${scoreboard['humanWins']} and Computer wins: ${scoreboard['computerWins']}`
+    
+    result.innerHTML =  resultsMessage;
+    return result;
+    }
 
 function getRndInteger() {
     return Math.floor(Math.random() * 3);
@@ -145,35 +126,13 @@ function getRndInteger() {
 function getComputerChoice() {
    
     let number = getRndInteger();
-
     if (number === 0) {
        return "rock"
     } else if (number === 1) {
         return "paper"
     } else {
-        return "scissors"}
-
-    
-};
-
-function getPlayerChoice (rounds) {
-    
-    let choice = "";
-
-    do {
-        choice = prompt(`Round ${rounds}. Please choose 'Rock', 'Paper' or 'Scissors'`).toLowerCase();
-        }
-    while 
-        (choice != "rock" && choice !="paper" && choice != "scissors");
-    
-    console.log("Player chooses - ", choice);
-    return choice;
-        
-    
-    
-    }
-     
-   
+        return "scissors"}   
+};    
 
 function playRound(playerSelection, computerSelection) {
 
@@ -219,60 +178,7 @@ function playRound(playerSelection, computerSelection) {
         else
         {
             return "T";
-    
         }        
     }
     }
 
-    // "You Lose! Paper beats Rock"
-    // "You Lose! Rock beats Scissors"
-    // "You Lose! Scissors beats Paper"
-    
-    // "You Win! Paper beats Rock"
-    // "You Win! Rock beats Scissors"
-    // "You Win! Scissors beats Paper"
-   
-function game() {
-
-    let ties = 0;
-    let playerwins = 0;
-    let computerwins = 0;
-
-    //for (let rounds = 1; rounds <6; rounds++) {
-        
-        let computerChoice = getComputerChoice();
-
-        let choice = getPlayerChoice(rounds);
-        
-        
-        winner = round(choice, computerChoice);
-
-        if (winner === "tie") {
-            ties++
-        } else if (winner == "player") {       
-        playerwins++ } else {
-            computerwins++
-        };
-
-        console.log("Round number - ", rounds, winner);
-
-    //}
-
-    console.log(`Player wins ${playerwins}, Computer wins ${computerwins} and ${ties} tie(s)`);
-    
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-//game();
-
-});
